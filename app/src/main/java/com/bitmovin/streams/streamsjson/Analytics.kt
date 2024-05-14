cf86e90450a5827552083ba3500538132424908b
noahkohrs <noah.kohrs@bitmovin.com>
2024-05-14 17:35:57 +0200

    feat: basic Analytics getters

--- app/src/main/java/com/bitmovin/streams/streamsjson/Analytics.kt
+++ app/src/main/java/com/bitmovin/streams/streamsjson/Analytics.kt
@@ -1,7 +1,16 @@
 package com.bitmovin.streams.streamsjson
 
+import com.bitmovin.analytics.api.AnalyticsConfig
+import com.bitmovin.player.api.analytics.AnalyticsPlayerConfig
+
 class Analytics {
-    lateinit var key: String
+    private lateinit var key: String
     lateinit var videoId: String
     lateinit var videoTitle: String
+
+    fun getAnalyticsConfig() : AnalyticsConfig {
+        return AnalyticsConfig(
+            licenseKey = key
+        )
+    }
 }
