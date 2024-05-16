77a52179f8cb19f3d96454198dec6d24db6e4850
noahkohrs <noah.kohrs@bitmovin.com>
2024-05-16 15:27:48 +0200

    fix: better Modifiers handling

---
+++ app/src/main/java/com/bitmovin/streams/FullScreen.kt
@@ -0,0 +1,8 @@
+package com.bitmovin.streams
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun FunScreen(content: @Composable () -> Unit) {
+    //TODO: Might be needed for a nice render of the fullscreen mode
+}
