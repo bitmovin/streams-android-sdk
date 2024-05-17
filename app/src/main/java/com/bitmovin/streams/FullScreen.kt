fbfb3cc0f800cb77887701a0a1f65cc4c103d2f4
noahkohrs <noah.kohrs@bitmovin.com>
2024-05-17 15:32:56 +0200

    feat: Home-made components to display immersive fullscreen (FINALLY!!!) + better separation of concerns

--- app/src/main/java/com/bitmovin/streams/FullScreen.kt
+++
@@ -1,8 +0,0 @@
-package com.bitmovin.streams
-
-import androidx.compose.runtime.Composable
-
-@Composable
-fun FunScreen(content: @Composable () -> Unit) {
-    //TODO: Might be needed for a nice render of the fullscreen mode
-}
