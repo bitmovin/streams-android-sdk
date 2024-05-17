4ec5d391cb0ba3c7c415d0ef82419b012956f4b6
noahkohrs <noah.kohrs@bitmovin.com>
2024-05-17 15:32:56 +0200

    feat: Home-made components to display immersive fullscreen (FINALLY!!!) + better separation of concerns

---
+++ app/src/main/java/com/bitmovin/streams/CustomComponents.kt
@@ -0,0 +1,137 @@
+package com.bitmovin.streams
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.AndroidView
+import com.bitmovin.player.PlayerView
+import com.bitmovin.player.SubtitleView
+import android.view.View
+import android.view.WindowManager
+import android.widget.FrameLayout
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
+
+
+/**
+ * Player wrapped into a compose element
+ */
+@Composable
+fun StreamVideoPlayer(playerView: PlayerView, subtitleView: SubtitleView, modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        AndroidView(factory = { playerView })
+        AndroidView(factory = { subtitleView })
+    }
+}
+
+
+/**
+ * Make the content fill the screen and show the status bar
+ * @param onDismissRequest: Callback to dismiss the dialog
+ * @param content: The content to display
+ *
+ * TODO: More flexible parameters to allow for more use-cases, but it's doing the job well for the video player
+ */
+@Composable
+fun FullScreen(
+    onDismissRequest: () -> Unit,
+    content: @Composable () -> Unit
+) {
+    Dialog(
+        onDismissRequest = onDismissRequest,
+        properties = DialogProperties(
+            dismissOnBackPress = true,
+            dismissOnClickOutside = false,
+            usePlatformDefaultWidth = false
+        ),
+        content = {
+                content()
+        }
+    )
+}
+
+
+/**
+ * Make the content fill the screen and hide the status bar
+ * (YouTube-like immersive full screen)
+ *
+ * @param onDismissRequest: Callback to dismiss the dialog
+ * @param content: The content to display
+ *
+ * TODO: More flexible parameters to allow for more use-cases, but it's doing the job well for the video player
+ */
+@Composable
+fun ImmersiveFullScreen(
+    onDismissRequest: () -> Unit,
+    content: @Composable () -> Unit
+) {
+    Dialog(
+        onDismissRequest = onDismissRequest,
+        properties = DialogProperties(
+            dismissOnBackPress = true,
+            dismissOnClickOutside = false,
+            usePlatformDefaultWidth = true,
+            decorFitsSystemWindows = false
+        ),
+        content = {
+            val activityWindow = getActivityWindow()
+            val dialogWindow = getDialogWindow()
+            val parentView = LocalView.current.parent as View
+
+            SideEffect {
+                if (activityWindow != null && dialogWindow != null) {
+                    val attributes = WindowManager.LayoutParams().apply {
+                        copyFrom(activityWindow.attributes)
+                        type = dialogWindow.attributes.type
+                    }
+                    dialogWindow.attributes = attributes
+                    parentView.layoutParams = FrameLayout.LayoutParams(activityWindow.decorView.width, activityWindow.decorView.height)
+                }
+            }
+
+            val context =  LocalContext.current
+            val window = getDialogWindow()
+
+            DisposableEffect(Unit) {
+                val originalStatusBarColor = window?.statusBarColor
+                window?.statusBarColor = Color.Transparent.toArgb()
+                window?.decorView?.systemUiVisibility =
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+
+                onDispose {
+                    if (originalStatusBarColor != null) {
+                        window.statusBarColor = originalStatusBarColor
+                    }
+                    window?.decorView?.systemUiVisibility = View.SYSTEM_UI_FLAG_VISIBLE
+                }
+            }
+
+            DisposableEffect(Unit) {
+                window?.decorView?.systemUiVisibility =
+                    (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            or View.SYSTEM_UI_FLAG_FULLSCREEN
+                            or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+
+                onDispose {
+                    window?.decorView?.systemUiVisibility = View.SYSTEM_UI_FLAG_VISIBLE
+                }
+            }
+
+            Surface(modifier = Modifier.fillMaxSize(), color = Color.Transparent) {
+                content()
+            }
+        }
+    )
+}
--- app/src/main/java/com/bitmovin/streams/Utils.kt
+++ app/src/main/java/com/bitmovin/streams/Utils.kt
@@ -1,8 +1,14 @@
 package com.bitmovin.streams
 
+import android.app.Activity
 import android.content.Context
+import android.content.ContextWrapper
 import android.util.Log
 import android.view.ViewGroup
+import android.view.Window
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.window.DialogWindowProvider
 import com.bitmovin.analytics.api.AnalyticsConfig
 import com.bitmovin.analytics.api.SourceMetadata
 import com.bitmovin.player.PlayerView
@@ -35,6 +41,19 @@ import okhttp3.Request
  * @throws IOException if the request fails
  */
 
+// Window utils, credits goes to the stackoverflow-guy
+@Composable
+fun getDialogWindow(): Window? = (LocalView.current.parent as? DialogWindowProvider)?.window
+
+@Composable
+fun getActivityWindow(): Window? = LocalView.current.context.getActivityWindow()
+
+private tailrec fun Context.getActivityWindow(): Window? =
+    when (this) {
+        is Activity -> window
+        is ContextWrapper -> baseContext.getActivityWindow()
+        else -> null
+    }
 
 
 suspend fun getStreamConfigData(streamId: String, jwToken: String?) : StreamConfigDataResponse {
