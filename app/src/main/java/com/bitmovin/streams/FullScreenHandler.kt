f88f71c2d9ff04eb3235ccc30cb79fc3977de9db
noahkohrs <noah.kohrs@bitmovin.com>
2024-05-22 14:22:02 +0200

    refactor: removing/moving unused code

--- app/src/main/java/com/bitmovin/streams/FullScreenHandler.kt
+++ app/src/main/java/com/bitmovin/streams/FullScreenHandler.kt
@@ -1,14 +1,11 @@
 package com.bitmovin.streams
-
-import android.app.Activity
 import android.content.Context
-import android.content.ContextWrapper
-import android.util.Log
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import com.bitmovin.player.api.ui.FullscreenHandler
 
 
-class FullScreenHandler(var fullscreen: MutableState<Boolean>, val context: Context) : FullscreenHandler {
+class FullScreenHandler(val context: Context, var fullscreen: MutableState<Boolean> = mutableStateOf(false)) : FullscreenHandler {
     override fun onDestroy() {
         TODO("Not yet implemented")
     }
@@ -24,50 +21,16 @@ class FullScreenHandler(var fullscreen: MutableState<Boolean>, val context: Cont
     }
 
     private fun handleFullScreen(b: Boolean) {
-        doSystemUiVisibility(b)
     }
 
     override fun onPause() {
+        TODO("Not yet implemented")
     }
 
     override fun onResume() {
-        if (isFullscreen) {
-            doSystemUiVisibility(true)
-        }
+        TODO("Not yet implemented")
     }
 
     override val isFullscreen: Boolean
         get() = fullscreen.value
-
-    /**
-     * This method toggles the system UI visibility.
-     * cf : hide the status bar and the navigation bar
-     */
-    private fun doSystemUiVisibility(fullscreen: Boolean) {
-        val activity = context.getActivity()
-        if (activity == null) {
-            Log.e("StreamsPlayer", "Activity not found")
-            return
-        }
-
-        //TODO: For the Immersive full screen mode, this will be needed, but for now it's useless
-
-
-
-    }
-}
-
-/**
- * Getting the Activity from a random Context
- */
-fun Context.getActivity(): Activity? {
-    val context = this
-    if (context is ContextWrapper) {
-        return if (context is Activity) {
-            context
-        } else {
-            context.baseContext.getActivity()
-        }
-    }
-    return null
 }
