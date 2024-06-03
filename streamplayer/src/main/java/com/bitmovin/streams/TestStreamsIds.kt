4fb6a22c75e38491e189ad14afd7a775a66289df
noahkohrs <noah.kohrs@bitmovin.com>
2024-06-03 10:25:52 +0200

    test: Making the video library example smoother + adding a temporary token hardcoded granting access the big bugs bunny video

--- streamplayer/src/main/java/com/bitmovin/streams/TestStreamsIds.kt
+++ streamplayer/src/main/java/com/bitmovin/streams/TestStreamsIds.kt
@@ -5,7 +5,7 @@ class TestStreamsIds {
         const val TEAR_OF_STEEL = "cot1gjsd1q7m56d4ch3g"
         const val SINTEL = "cotpqicd1q7m56d4chgg"
         const val VERTICAL_VIDEO = "cp7jaalsrjnnig8jcdj0"
-        const val SQUARE_VIDEO = "cp7jf3cd1q7m56d4copg"
+        const val SQUARE_VIDEO = "cpeno8tsrjnnig8jcn60"
         const val BIG_BUCK_BUNNY = "cp1mv7dsrjnnig8jc960" // NEED THE TOKEN
     }
 }
