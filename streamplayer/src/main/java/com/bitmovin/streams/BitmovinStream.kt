ca23fa279d6142fb9f59344c3a4208a231151945
noahkohrs <noah.kohrs@bitmovin.com>
2024-06-07 11:10:01 +0200

    Making the property bubbles more precise on the demo app

--- streamplayer/src/main/java/com/bitmovin/streams/BitmovinStream.kt
+++ streamplayer/src/main/java/com/bitmovin/streams/BitmovinStream.kt
@@ -65,7 +65,7 @@ BitmovinStream(
  * @param subtitles The list of subtitle tracks available for the stream.
  * @param immersiveFullScreen Whether the player should be in immersive full screen mode. Recommended to be false if the EdgeToEdge is disabled (may break on some devices).
  * @param bitmovinStreamEventListener The listener for the player events.
- * @param screenOrientationOnFullscreenEscape The screen orientation to be set when the player exits full screen. If null, the screen orientation will automatically reset to the state before entering full screen.
+ * @param appDefaultOrientation The screen orientation to be set when the player exits full screen. If null, the screen orientation will automatically reset to the state before entering full screen.
  * @param enableAds Whether ads should be enabled.
  * @param styleConfig The style configuration for the player. This property has priority over the style configuration from the dashboard.
  */
