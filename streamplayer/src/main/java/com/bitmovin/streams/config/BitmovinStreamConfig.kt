6c1a01f0b34e836fd33a839ab288c7548d9fbd64
noahkohrs <noah.kohrs@bitmovin.com>
2024-06-07 11:28:38 +0200

    chore: config files made more flexible

--- streamplayer/src/main/java/com/bitmovin/streams/config/BitmovinStreamConfig.kt
+++ streamplayer/src/main/java/com/bitmovin/streams/config/BitmovinStreamConfig.kt
@@ -15,10 +15,48 @@ data class BitmovinStreamConfig(
     var start : Double = 0.0,
     var subtitles : List<SubtitleTrack> = emptyList(),
     var immersiveFullScreen : Boolean = true,
-    var onPlayerReady : (player: Player) -> Unit = {},
-    var onPlayerViewReady : (playerView: PlayerView) -> Unit = {},
+    var streamEventListener: BitmovinStreamEventListener? = null,
     var appDefaultOrientation: Int? = null,
     var enableAds : Boolean = true,
     var styleConfig : StyleConfigStream = StyleConfigStream()
 ) {
+
+    constructor(
+        streamId : String,
+        modifier : Modifier = Modifier,
+        jwToken : String? = null,
+        autoPlay : Boolean = false,
+        muted : Boolean = false,
+        poster : String? = null,
+        start : Double = 0.0,
+        subtitles : List<SubtitleTrack> = emptyList(),
+        immersiveFullScreen : Boolean = true,
+        onPlayerReady : (Player) -> Unit,
+        onPlayerViewReady : (PlayerView) -> Unit,
+        appDefaultOrientation: Int? = null,
+        enableAds : Boolean = true,
+        styleConfig : StyleConfigStream = StyleConfigStream()
+    ) : this(
+        streamId,
+        modifier,
+        jwToken,
+        autoPlay,
+        muted,
+        poster,
+        start,
+        subtitles,
+        immersiveFullScreen, object :
+            BitmovinStreamEventListener {
+                override fun onPlayerReady(player: Player) {
+                    onPlayerReady(player)
+                }
+
+                override fun onPlayerViewReady(playerView: PlayerView) {
+                    onPlayerViewReady(playerView)
+                }
+            },
+        appDefaultOrientation,
+        enableAds,
+        styleConfig
+    )
 }
