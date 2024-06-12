cb600fbbc2ababf611b585de017bb10d966dfa03
noahkohrs <noah.kohrs@bitmovin.com>
2024-06-12 15:07:27 +0200

    test: updating the fragment test app

--- testapp/src/main/java/com/bitmovin/testapp/FragmentTest.kt
+++ testapp/src/main/java/com/bitmovin/testapp/FragmentTest.kt
@@ -8,36 +8,16 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import com.bitmovin.testapp.databinding.ActivityViewFragBinding
-
+import com.bitmovin.testapp.databinding.ActivityViewBinding
 class FragmentTest : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityViewFragBinding
+    private lateinit var binding: ActivityViewBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        enableEdgeToEdge()
-        binding = ActivityViewFragBinding.inflate(layoutInflater)
+        binding = ActivityViewBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(binding.toolbar)
-
-        val navController = findNavController(R.id.nav_host_fragment_content_view_frag)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null)
-                .setAnchorView(R.id.fab).show()
-        }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_view_frag)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
     }
 }
