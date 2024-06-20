cf4cabba88a8d64b5f109a6b65fc7df5ab75b407
noahkohrs <noah.kohrs@bitmovin.com>
2024-06-20 10:42:11 +0200

    test: adding simple unit test to verify the config generated is correct.

---
+++ streamplayer/src/test/java/com/bitmovin/streams/CorrectnessTest.kt
@@ -0,0 +1,69 @@
+package com.bitmovin.streams
+
+import androidx.compose.ui.graphics.Color
+import com.bitmovin.player.api.advertising.AdSourceType
+import com.bitmovin.streams.streamsjson.StreamConfigData
+import com.google.gson.Gson
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class CorrectnessTest {
+
+    @Test
+    fun createAdsFromStreamData() {
+        val streamData = Gson().fromJson(tear_of_steel, StreamConfigData::class.java)
+
+        val adConfig = createAdvertisingConfig(
+            streamData
+        )
+        assert(adConfig.schedule.size == 3)
+        adConfig.schedule.forEachIndexed { index, ad ->
+            assertEquals(ad.position, streamData.adConfig.ads[index].position)
+            assertEquals(ad.sources[0].tag, streamData.adConfig.ads[index].url)
+            when (index) {
+                0 -> assertEquals(ad.sources[0].type, AdSourceType.Progressive)
+                1 -> assertEquals(ad.sources[0].type, AdSourceType.Bitmovin)
+                2 -> assertEquals(ad.sources[0].type, AdSourceType.Bitmovin)
+            }
+        }
+    }
+
+    /**
+     * Tests quickly Color.toCSS() & String.parseColor()
+     * Might be useful to delegate to a library to handle every case
+     */
+    @Test
+    fun colorCssNative() {
+        val color = Color(255, 0, 0, 255)
+        assertEquals(color.toCSS(), "rgba(255, 0, 0, 1.0)")
+        assertEquals(Color.parseColor("rgba(255, 0, 0, 1.0)"), color)
+    }
+
+    @Test
+    fun createSourceConfigFromStreamData() {
+        val streamData = Gson().fromJson(tear_of_steel, StreamConfigData::class.java)
+
+        val source = createSourceConfig(
+            streamData,
+            null
+        )
+        assertEquals(source.url, streamData.sources.hls)
+        assertEquals(source.posterSource, streamData.sources.poster)
+        assertEquals(source.thumbnailTrack?.url, streamData.sources.thumbnailTrack.url)
+        assertEquals(source.title, streamData.sources.title)
+    }
+
+    @Test
+    fun createSourceMetaDataFromStreamData() {
+        val streamData = Gson().fromJson(tear_of_steel, StreamConfigData::class.java)
+
+        val meta = createMetadata(
+            streamData
+        )
+
+        assertEquals(meta.videoId, streamData.analytics.videoId)
+        assertEquals(meta.title, streamData.analytics.videoTitle)
+        assertEquals(meta.isLive, streamData.isLive())
+    }
+}
---
+++ streamplayer/src/test/java/com/bitmovin/streams/example_data.kt
@@ -0,0 +1,53 @@
+package com.bitmovin.streams
+
+val tear_of_steel = """
+    {
+      "key": "_________________________",
+      "sources": {
+        "title": "Tears of Steel",
+        "hls": "https://streams.bitmovin.com/cot1gjsd1q7m56d4ch3g/manifest.m3u8",
+        "poster": "https://streams.bitmovin.com/cot1gjsd1q7m56d4ch3g/poster",
+        "thumbnailTrack": {
+          "url": "https://cdn.bitmovin.com/content/assets/streams-sample-video/tos/sprites/low/sprite.vtt"
+        }
+      },
+      "analytics": {
+        "key": "_________________________",
+        "videoId": "cot1gjsd1q7m56d4ch3g",
+        "videoTitle": "Tears of Steel"
+      },
+      "type": "VIDEO",
+      "styleConfig": {
+        "playerStyle": {
+          "playbackMarkerBgColor": "rgba(248, 250, 252, 1)",
+          "playbackMarkerBorderColor": "rgba(47, 66, 74, 1)",
+          "playbackTrackPlayedColor": "rgba(140, 28, 113, 1)",
+          "playbackTrackBufferedColor": "rgba(168, 255, 171, 1)",
+          "playbackTrackBgColor": "rgba(255, 150, 150, 0.35)",
+          "textColor": "rgba(61, 219, 255, 1)",
+          "backgroundColor": "rgba(176, 40, 40, 1)"
+        },
+        "watermarkUrl": "https://streams-resources.s3.eu-west-1.amazonaws.com/watermark/017906b0-1031-4457-9edb-e3d12df645b9.jpg",
+        "watermarkTargetLink": "https://dashboard.bitmovin.com/streams/video/cp7jaalsrjnnig8jcdj0?tabId=2&orgId=59c5fb25-6901-4354-a37d-1dfb9bf9b17c&userId=9ebfa014-8812-43d6-8c21-9dcf2e980552"
+      },
+      "adConfig": {
+        "ads": [
+          {
+            "position": "post",
+            "url": "https://bitmovin-a.akamaihd.net/content/testing/ads/testad2s.mp4",
+            "type": "vast"
+          },
+          {
+            "position": "pre",
+            "url": "https://file-examples.com/storage/fed5266c9966708dcaeaea6/2018/04/file_example_MOV_480_700kB.mov",
+            "type": "vast"
+          },
+          {
+            "position": "50%",
+            "url": "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dskippablelinear&correlator=",
+            "type": "vast"
+          }
+        ]
+      }
+    }
+""".trimIndent()
