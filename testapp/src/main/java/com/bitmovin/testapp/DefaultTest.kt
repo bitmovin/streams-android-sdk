d246aa2ab6f7511d0069deeb544c007bfd254b07
noahkohrs <noah.kohrs@bitmovin.com>
2024-06-25 10:44:06 +0200

    test: simple testapp (need to add the content)

--- testapp/src/main/java/com/bitmovin/testapp/DefaultTest.kt
+++ testapp/src/main/java/com/bitmovin/testapp/DefaultTest.kt
@@ -1,69 +1,55 @@
 package com.bitmovin.testapp
 
-import android.os.Bundle
 import androidx.activity.ComponentActivity
+import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.bitmovin.player.api.media.subtitle.SubtitleTrack
 import com.bitmovin.streams.BitmovinStream
-import com.bitmovin.streams.config.StyleConfigStream
 import com.bitmovin.streams.TestStreamsIds
+import com.bitmovin.streams.config.BitmovinStreamConfig
 
 class DefaultTest : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Column {
+            val scrollState = rememberScrollState()
+            var selectedConfig by remember { mutableStateOf(TESTCASES_ERRORS.keys.first()) }
 
-                Box(
-                    modifier = Modifier
-                        .size(300.dp)
-                        .padding(20.dp)
-                ) {
-                    Column {
-                        Text(text = "Video 1")
-                        BitmovinStream(
-                            streamId = TestStreamsIds.VERTICAL_VIDEO,
-                            subtitles = emptyList(),
-                            modifier = Modifier,
-                            styleConfig = StyleConfigStream(
-                                customCss = """
-                                            div {
-                                              font-size: 15pt !important;
-                                            }
-                                        """.trimIndent()
-                            )
-                        )
-
+            Column(Modifier.safeDrawingPadding()) {
+                BitmovinStream(config = TESTCASES_ERRORS[selectedConfig]!!)
+                Text(text = "Error messages Tests", style = MaterialTheme.typography.titleLarge, modifier = Modifier.padding(4.dp))
+                Row(
+                    Modifier
+                        .horizontalScroll(scrollState)) {
+                    TESTCASES_ERRORS.keys.forEach { name ->
+                        Button(
+                            onClick = { selectedConfig = name },
+                            modifier = Modifier.padding(4.dp)
+                        ) {
+                            Text(text = name)
+                        }
                     }
                 }
-
-                BitmovinStream(
-                    streamId = TestStreamsIds.SQUARE_VIDEO,
-                    modifier = Modifier
-                        .size(300.dp)
-                        .padding(20.dp),
-                    enableAds = false,
-                    subtitles = listOf(
-                        SubtitleTrack(
-                            language = "French",
-                            url = "https://cdn.bitmovin.com/content/assets/sintel/subtitles/subtitles_fr.vtt"
-                        ),
-                        SubtitleTrack(
-                            language = "German",
-                            url = "https://cdn.bitmovin.com/content/assets/sintel/subtitles/subtitles_de.vtt"
-                        ),
-                    )
-                )
             }
         }
     }
 }
+
+val TESTCASES_ERRORS = mapOf(
+    "404 (Stream not found)" to BitmovinStreamConfig(streamId = TestStreamsIds.SQUARE_VIDEO+"d"),
+    "401 (Unauthorized)" to BitmovinStreamConfig(streamId = TestStreamsIds.BIG_BUCK_BUNNY),
+    "403 (Forbidden)" to BitmovinStreamConfig(streamId = TestStreamsIds.BIG_BUCK_BUNNY, jwToken = "nonvalid"),
+    // Add more test cases as needed
+)
+
