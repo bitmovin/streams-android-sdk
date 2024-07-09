a95dcc37abb37f5a51f244dd5eff4471adb07f33
noahkohrs <noah.kohrs@bitmovin.com>
2024-07-09 10:27:12 +0200

    refactor: the config classes not dataclasses anymore.

--- streams/src/main/java/com/bitmovin/streams/config/FullscreenConfig.kt
+++ streams/src/main/java/com/bitmovin/streams/config/FullscreenConfig.kt
@@ -3,29 +3,29 @@ package com.bitmovin.streams.config
 /**
  * Configuration for the fullscreen behavior of the player.
  */
-public data class FullscreenConfig(
+public class FullscreenConfig(
     /**
      * Whether the fullscreen behavior should be enabled.
      */
-    var enable: Boolean = true,
+    public var enable: Boolean = true,
     /**
      * Whether the player should be in immersive mode.
      *
      * Recommended to be false if the _EdgeToEdge_ is disabled on the Activity (may break on some very specific devices).
      */
-    var immersive: Boolean = true,
+    public var immersive: Boolean = true,
     /**
      * Whether the player should automatically change orientation.
      */
-    var autoRotate: Boolean = true,
+    public var autoRotate: Boolean = true,
     /**
      * The minimum aspect ratio for forcing landscape orientation. Does not apply if autoRotate is false.
      */
-    var minAspectRatioForLandscapeForce: Float = 1.2f,
+    public var minAspectRatioForLandscapeForce: Float = 1.2f,
     /**
      * The maximum aspect ratio for forcing portrait orientation. Does not apply if autoRotate is false.
      */
-    var maxAspectRatioForPortraitForce: Float = 0.8f,
+    public var maxAspectRatioForPortraitForce: Float = 0.8f,
     /**
      * The default orientation of the screen.
      *
@@ -33,11 +33,11 @@ public data class FullscreenConfig(
      *
      * Should correspond to one of the ActivityInfo.SCREEN_ORIENTATION_* constants.
      */
-    var screenDefaultOrientation: Int? = null,
+    public var screenDefaultOrientation: Int? = null,
     /**
      * Whether the player should automatically enter picture-in-picture mode when the app goes to the background while a stream being in fullscreen.
      */
-    var autoPiPOnBackground: Boolean = true,
+    public var autoPiPOnBackground: Boolean = true,
 ) {
     public companion object {
         public val DEFAULT: FullscreenConfig = FullscreenConfig()
